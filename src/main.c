95055191ccf00268d8bc56e71b3abf5c5af29710
zacholski <p.zacholski@wp.pl>
2022-07-20 10:37:18 +0000

    reading from file added, starting arrange the data

--- src/main.c
+++ src/main.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+
+
+//proc/stat/ file
+FILE *procStatFile;
+
+
+
+//Variables
+
+
+
+//Threads
+pthread_t reader,analyzer,printer,watchdog,logger;
+
+
+//Main functions
+
+void* read_data(void* args)
+{
+    if((procStatFile=fopen("proc/stat","r"))==NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    char* whichLine = NULL;
+    size_t length = 0;
+    ssize_t line = 0;
+
+    while((line=getline(&whichLine,&length,procStatFile))!= -1)
+    {
+        
+        
+    } 
+    
+}
+
+
+void clearAll()
+{
+    //clearing pthreaders
+    pthread_join(reader,NULL);
+    pthread_join(analyzer,NULL);
+    pthread_join(printer,NULL);
+    pthread_join(watchdog,NULL);
+    pthread_join(logger,NULL);
+}
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    pthread_create(&reader,NULL,&read_data,NULL);
+    clearAll();
+    return EXIT_SUCCESS;
 }
